b2b211bb566245ab57cd7225a520a86779c38b02
dejun.xdj <dejun.xdj@alibaba-inc.com>
2018-12-29 14:54:05 +0800

    Change SDS_HDR_VAR macro to a properer definition.
    Avoid type conversion error in c++.

--- sds.h
+++ sds.h
@@ -80,8 +80,8 @@ struct __attribute__ ((__packed__)) sdshdr64 {
 #define SDS_TYPE_64 4
 #define SDS_TYPE_MASK 7
 #define SDS_TYPE_BITS 3
-#define SDS_HDR_VAR(T,s) struct sdshdr##T *sh = (void*)((s)-(sizeof(struct sdshdr##T)));
 #define SDS_HDR(T,s) ((struct sdshdr##T *)((s)-(sizeof(struct sdshdr##T))))
+#define SDS_HDR_VAR(T,s) struct sdshdr##T *sh = SDS_HDR(T,s)
 #define SDS_TYPE_5_LEN(f) ((f)>>SDS_TYPE_BITS)
 
 static inline size_t sdslen(const sds s) {
