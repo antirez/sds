8da2034fce1fdc6f1ebad5105b0dd2f8747df513
IIOL <islamov.igor.98@gmail.com>
2020-01-04 03:49:37 +0300

    Fixed sdscat*() bug: read memory error

--- sds.c
+++ sds.c
@@ -399,7 +399,7 @@ sds sdscatlen(sds s, const void *t, size_t len) {
 
     s = sdsMakeRoomFor(s,len);
     if (s == NULL) return NULL;
-    memcpy(s+curlen, t, len);
+    memmove(s+curlen, t, len);
     sdssetlen(s, curlen+len);
     s[curlen+len] = '\0';
     return s;
